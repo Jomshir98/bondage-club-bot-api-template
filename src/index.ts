f18c125abe979696f058f3612cb23c2cbef1d9e9
jomshir98 <jomshir98@protonmail.com>
2021-10-17 00:12:42 +0200

    Update to new logger

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,67 @@
-import { Init, logger, Connect, AssetGet, logConfig, LogLevel } from "bondage-club-bot-api";
+import { AssetGet, Connect, Init, logger, AddFileOutput, LogLevel, SetConsoleOutput, logConfig } from "bondage-club-bot-api";
 import { TestLogic } from "./testLogic";
 import { USERNAME, PASSWORD } from "./secrets";
 
-import fsPromises from "fs/promises";
+import * as fs from "fs";
 
 // To reduce loglevel, change it here
-logConfig.logLevel = LogLevel.DEBUG;
-
-let conn: API_Connector | null = null;
+SetConsoleOutput(LogLevel.DEBUG);
 
 const time = new Date();
 const timestring = `${time.getFullYear() % 100}${(time.getMonth() + 1).toString().padStart(2, "0")}${time.getDate().toString().padStart(2, "0")}_` +
 	`${time.getHours().toString().padStart(2, "0")}${time.getMinutes().toString().padStart(2, "0")}`;
 const logPrefix = `${timestring}_${process.pid}`;
 
-fsPromises
-	.mkdir("./data/logs", { recursive: true })
-	.then(() => fsPromises.open(`./data/logs/${logPrefix}_debug.log`, "w"))
-	.then(log => logger.addFileOutput(LogLevel.DEBUG, log))
-	.then(() => fsPromises.open(`./data/logs/${logPrefix}_error.log`, "as"))
-	.then(log => logger.addFileOutput(LogLevel.ALERT, log))
-	.then(Init)
-	.then(async () => {
-		conn = await Connect(USERNAME, PASSWORD);
+fs.mkdirSync(`./data/logs`, { recursive: true });
+AddFileOutput(`./data/logs/${logPrefix}_debug.log`, false, LogLevel.DEBUG);
+AddFileOutput(`./data/logs/${logPrefix}_error.log`, true, LogLevel.ALERT);
 
-		const testLogic = new TestLogic();
-		conn.logic = testLogic;
+let conn: API_Connector | null = null;
+let testLogic: TestLogic | null = null;
 
+async function run() {
+	conn = await Connect(USERNAME, PASSWORD);
 
-		// These just expose some things in debug console
-		// @ts-ignore: dev
-		global.AssetGet = AssetGet;
-		// @ts-ignore: dev
-		global.conn = conn;
-		// @ts-ignore: dev
-		global.logic = testLogic;
+	testLogic = new TestLogic();
+	conn.logic = testLogic;
 
-		// To work properly bot *needs* to be room admin!
-		await conn.ChatRoomJoinOrCreate({
-			Name: "Bot test",
-			Description: "[BOT] Testing room for jomshir's bots",
-			Background: "MainHall",
-			Limit: 10,
-			Private: true,
-			Locked: false,
-			Admin: [conn.Player.MemberNumber],
-			Ban: [],
-			Game: "",
-			BlockCategory: ["Leashing"]
-		});
+	// These just expose some things in debug console
+	// @ts-ignore: dev
+	global.AssetGet = AssetGet;
+	// @ts-ignore: dev
+	global.conn = conn;
+	// @ts-ignore: dev
+	global.logic = testLogic;
 
-		logger.alert("Ready!");
-	}).catch(err => {
+	// To work properly bot *needs* to be room admin!
+	await conn.ChatRoomJoinOrCreate({
+		Name: "Bot test",
+		Description: "[BOT] Testing room for jomshir's bots",
+		Background: "MainHall",
+		Limit: 10,
+		Private: true,
+		Locked: false,
+		Admin: [conn.Player.MemberNumber],
+		Ban: [],
+		Game: "",
+		BlockCategory: ["Leashing"]
+	});
+
+	logger.alert("Ready!");
+}
+
+Init()
+	.then(run, err => {
+		logger.fatal("Asset loading rejected:", err);
+	})
+	.catch(err => {
 		logger.fatal("Error while running:", err);
 	});
 
-logger.onfatal(() => {
+logConfig.onFatal.push(() => {
 	conn?.disconnect();
 	conn = null;
+	testLogic = null;
 });
 
 process.once("SIGINT", () => {
